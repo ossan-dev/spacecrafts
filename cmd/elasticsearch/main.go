1ade29dc14a292222a44a64f87ff6ef8360508b3
ossan-dev <ivan.pesenti.dev@gmail.com>
2023-11-29 15:12:37 +0100

    introduced flags

--- cmd/elasticsearch/main.go
+++ cmd/elasticsearch/main.go
@@ -2,30 +2,40 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"flag"
 
 	"spacecraft/domain"
 	"spacecraft/logic"
 )
 
+// docker run -p 9200:9200 -it -m 1GB docker.elastic.co/elasticsearch/elasticsearch:8.11.1
+// go run . -mode=sync
 func main() {
 	ctx := context.Background()
-	// ctx = logic.ConnectWithElasticSearch(ctx)
-	ctx, err := logic.LoadspacecraftAsync(ctx)
+	var err error
+	modePtr := flag.String("mode", "mode", `specify how to run the program ("sync" or "async"). The latter is the default"`)
+	flag.Parse()
+	if modePtr != nil && *modePtr == "sync" {
+		ctx, err = logic.Loadspacecraft(ctx)
+	} else {
+		ctx, err = logic.LoadspacecraftAsync(ctx)
+	}
 	if err != nil {
 		panic(err)
 	}
-	// ctx, err := logic.Loadspacecraft(ctx)
-	// if err != nil {
-	// 	panic(err)
-	// }
 	spacecraft := ctx.Value(domain.ModelsKey).([]*domain.Spacecraft)
 	if spacecraft == nil {
 		panic("no spacecraft in context")
 	}
+	ctx = logic.ConnectWithElasticSearch(ctx)
+	if err != nil {
+		panic(err)
+	}
+	_ = ctx
+	/******************* Debug ****************************/
 	// if err := internal.WritespacecraftToFile("domain/spacecraft.json", spacecraft); err != nil {
 	// 	fmt.Println(fmt.Errorf("WritespacecraftToFile() err: %v", err))
 	// }
 	// internal.Printspacecraft(os.Stdout, spacecraft)
-	fmt.Println("number of spacecraft:", len(spacecraft))
+	/******************* End of Debug ******************/
 }
