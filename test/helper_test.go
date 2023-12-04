e53d79d19cfca77090d635069cb4ab7ce033fd55
ossan-dev <ivan.pesenti.dev@gmail.com>
2023-12-04 13:59:20 +0100

    introduced suite

---
+++ test/helper_test.go
@@ -0,0 +1,53 @@
+package test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/wait"
+)
+
+type ITSuite struct {
+	suite.Suite
+	Endpoint  string
+	Ctx       context.Context
+	Container testcontainers.Container
+}
+
+func (s *ITSuite) SetupSuite() {
+	var err error
+	s.Ctx = context.Background()
+	req := testcontainers.ContainerRequest{
+		FromDockerfile: testcontainers.FromDockerfile{
+			Context: "../",
+		},
+		ExposedPorts: []string{"8080/tcp"},
+		WaitingFor:   wait.ForExec([]string{"/webserver"}),
+	}
+	s.Container, err = testcontainers.GenericContainer(
+		s.Ctx,
+		testcontainers.GenericContainerRequest{
+			ContainerRequest: req,
+			Started:          true,
+		},
+	)
+	require.Nil(s.T(), err)
+	endpoint, err := s.Container.Endpoint(s.Ctx, "")
+	require.Nil(s.T(), err)
+	s.Endpoint = endpoint
+}
+
+func (s *ITSuite) TearDownSuite() {
+	if s.Container != nil {
+		if err := s.Container.Terminate(s.Ctx); err != nil {
+			require.Nil(s.T(), err)
+		}
+	}
+}
+
+func TestSuite(t *testing.T) {
+	suite.Run(t, new(ITSuite))
+}
--- test/load_spacecrafts_test.go
+++ test/load_spacecrafts_test.go
@@ -3,49 +3,20 @@ package test
 import (
 	"context"
 	"fmt"
-	"testing"
 
 	"spacecraft/domain"
 	"spacecraft/logic/webclient"
 
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"github.com/testcontainers/testcontainers-go"
-	"github.com/testcontainers/testcontainers-go/wait"
 )
 
-func TestLoadspacecraft(t *testing.T) {
-	// arrange
-	ctx := context.Background()
-	req := testcontainers.ContainerRequest{
-		FromDockerfile: testcontainers.FromDockerfile{
-			Context: "../",
-		},
-		ExposedPorts: []string{"8080/tcp"},
-		WaitingFor:   wait.ForExec([]string{"/webserver"}),
-	}
-	container, err := testcontainers.GenericContainer(
-		ctx,
-		testcontainers.GenericContainerRequest{
-			ContainerRequest: req,
-			Started:          true,
-		},
-	)
-	require.Nil(t, err)
-	endpoint, err := container.Endpoint(ctx, "")
-	require.Nil(t, err)
-
+func (s *ITSuite) TestLoadspacecraft() {
 	// act
-	ctx, err = webclient.Loadspacecraft(ctx, fmt.Sprintf("http://%v", endpoint))
+	ctx, err := webclient.Loadspacecraft(context.Background(), fmt.Sprintf("http://%v", s.Endpoint))
 
 	// assert
-	require.Nil(t, err)
+	require.Nil(s.T(), err)
 	spacecraft, ok := ctx.Value(domain.ModelsKey).([]*domain.Spacecraft)
-	assert.True(t, ok)
-	assert.NotEmpty(t, spacecraft)
-
-	// teardown
-	if err := container.Terminate(ctx); err != nil {
-		t.Fatal(err)
-	}
+	s.True(ok)
+	s.NotEmpty(spacecraft)
 }
