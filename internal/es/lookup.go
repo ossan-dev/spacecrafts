23fe5ce4b65ee3accaa28142dde3475c1a873099
ossan-dev <ivan.pesenti.dev@gmail.com>
2023-12-20 11:52:41 +0100

    refactored lookup func

--- internal/es/lookup.go
+++ internal/es/lookup.go
@@ -17,19 +17,16 @@ func GetByID(esClient *elasticsearch.Client, index, documentID string) (*domain.
 		return nil, fmt.Errorf("err while looking up the document with ID: %v with err: %w", documentID, err)
 	}
 	defer res.Body.Close()
-	if res.StatusCode != http.StatusOK {
-		data, err := io.ReadAll(res.Body)
-		if err != nil {
-			// nit: here we lose the not 200 error context
-			return nil, fmt.Errorf("err while reading response body: %w", err)
+	data, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, fmt.Errorf("err while reading response body: %w", err)
+	}
+	if res.StatusCode == http.StatusOK {
+		var lookupRes domain.LookupResponse
+		if err = json.Unmarshal(data, &lookupRes); err != nil {
+			return nil, fmt.Errorf("failed to unmarshal the elastic search result: %w", err)
 		}
-		return nil, fmt.Errorf("err with the Elasticsearch request processing: %v", string(data))
+		return lookupRes.Source, nil
 	}
-	var lookupRes domain.LookupResponse
-	// a note on correct usage of json.NewDecoder:
-	// https://mottaquikarim.github.io/dev/posts/you-might-not-be-using-json.decoder-correctly-in-golang/
-	if err = json.NewDecoder(res.Body).Decode(&lookupRes); err != nil {
-		return nil, fmt.Errorf("failed to decode the elastic search result: %w", err)
-	}
-	return lookupRes.Source, nil
+	return nil, fmt.Errorf("failed to lookup doc: %w", fmt.Errorf(string(data)))
 }
